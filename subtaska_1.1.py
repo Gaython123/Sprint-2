606b36f8f4771b3b7492d8ce3f8c8b29ae61f992
lekorda <lesik2709@gmail.com>
2024-09-14 00:11:07 +0200

    evrthn but no many keys

--- subtaska_1.1.py
+++ subtaska_1.1.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+list_of_tasks = {}
+
+
+while True:
+    print()
+    user_command = input("print 'help' to see all options")
+
+
+    match user_command:
+        case 'help':
+            print('- help -> get the list of the commands \n1 -> Add a new task \n2 -> View all tasks \n3 -> Delete a task \n4 -> Exit')
+
+        case '1':
+            new_task = input("Create a new task")
+            key = 1
+            value = new_task
+            list_of_tasks[key] = value
+
+        case '2':
+            print(f"{list_of_tasks}")
+
+        case '3':
+            a = int(input("Please input the number of the task"))
+            list_of_tasks.pop(1)
+
+        case '4':
+            break
